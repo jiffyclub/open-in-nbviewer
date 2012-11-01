53ca7a7597532d773d69e746a7318aaa048ad571
Matt Davis <jiffyclub@gmail.com>
2012-10-31 20:21:17 -0400

    Adding the bookmarklet to the repo.

---
+++ bookmarklet/README.md
@@ -0,0 +1,7 @@
+Open in nbviewer Bookmarklet
+============================
+
+A bookmarklet that tries to open the current page in nbviewer.
+To use make a new bookmark in your browser and copy the code from
+[nbviewer_bookmarklet.js](https://github.com/jiffyclub/open-in-nbviewer/blob/master/bookmarklet/background.js)
+into the URL field. White space has been trimmed for brevity.
---
+++ bookmarklet/nbviewer_bookmarklet.js
@@ -0,0 +1,14 @@
+javascript:date=new Date();
+url_root='http://nbviewer.ipython.org/';
+url=null;
+if (location.href.search(/^https?:\/\/gist\.github\.com\/[0-9]+$/) !== -1) {
+gist = location.href.match(/^https?:\/\/gist\.github\.com\/([0-9]+)$/);
+url = url_root + gist[1];
+} else if (location.href.search(/^https:\/\/.*\.ipynb$/) !== -1) {
+path = location.href.match(/^https:\/\/(.*\.ipynb)$/);
+url = url_root + 'urls/' + path[1];
+} else if (location.href.search(/^http:\/\/.*\.ipynb$/) !== -1) {
+path = location.href.match(/^http:\/\/(.*\.ipynb)$/);
+url = url_root + 'url/' + path[1];}
+if (url) {
+void(window.open(url, 'nbviewer' + date.getTime()));}
