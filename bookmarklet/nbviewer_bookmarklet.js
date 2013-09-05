0613fefdb2137b6743930642994b986e03c34a22
David P. Sanders <dpsanders@gmail.com>
2013-09-05 00:49:10 -0500

    Removed console.log

--- bookmarklet/nbviewer_bookmarklet.js
+++ bookmarklet/nbviewer_bookmarklet.js
@@ -11,13 +11,9 @@ else {
 			
 	if (path != null) {
 		path = location.href;
-		console.log(path);
 		path = path.replace("/blob", "");
-		console.log(path);
 		path = path.replace("https://", "urls/raw.");
-		console.log(path);
 		url = url_root + path;
-		console.log(url);
 	}
 
 	else {
